19dbd7ec061d0db3b7e7e4bd96043fcfce3c004a
joyous <mascot417@126.com>
2017-12-15 11:09:26 +0800

    序列化测试

---
+++ src/main/com/test/SerializableTest/PersonTest.java
@@ -0,0 +1,65 @@
+package main.com.test.SerializableTest;
+
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+import java.io.*;
+
+public class PersonTest implements Serializable {
+
+    private static final long serialVersionUID = 1614918028946841050L;
+
+    private String name;
+
+    private Integer age;
+
+    private Integer sex;
+    private Integer sex1;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        //测试开关
+        Boolean serialBoll = false;
+
+        PersonTest personTest = new PersonTest();
+        personTest.setName("测试序列化");
+
+        File outFile = new File("D://personTest.text");
+        try {
+            if (!outFile.exists()) {
+                outFile.createNewFile();
+            }
+
+            if (serialBoll) {
+                //序列化到本地
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(outFile));
+                objectOutputStream.writeObject(personTest);
+                objectOutputStream.close();
+            } else {
+                //从本地序列化读取
+                ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(outFile));
+                PersonTest getPerson = (PersonTest) objectInputStream.readObject();
+                System.out.print(getPerson.sex1);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
