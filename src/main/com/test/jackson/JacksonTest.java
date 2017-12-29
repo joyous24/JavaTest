8753c339743ee325b54964d270be95807e48b97b
joyous <mascot417@126.com>
2017-12-29 18:36:38 +0800

    xml测试

--- src/main/com/test/jackson/JacksonTest.java
+++ src/main/com/test/jackson/JacksonTest.java
@@ -1,9 +1,21 @@
 package com.test.jackson;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.test.SerializableTest.PersonTest;
+
 /**
  * 转换对象为XML
  */
 public class JacksonTest {
 
+    public static void main(String[] args) throws JsonProcessingException {
+        PersonTest person = new PersonTest();
+        person.setAge(1);
 
+
+        ObjectMapper mapper = new XmlMapper();
+        System.out.println(mapper.writeValueAsString(person));
+    }
 }
