1df76d62fd8afbb77acd1a04afde42a1e98f0304
zhaoxiqing <zhaoxiqing@zhen.com>
2019-04-02 09:30:25 +0800

    补充学习实例

--- src/main/com/test/Test.java
+++ src/main/com/test/Test.java
@@ -1,21 +1,41 @@
-package main.com.test;
+package com.test;
 
+import com.zhen.commons.util.DateUtil;
 
-import com.amazonaws.mws.MwsUtl;
-
-import java.util.Date;
-import java.util.GregorianCalendar;
+import java.nio.DoubleBuffer;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Test {
 
-    public static void main(String[] args) {
-        GregorianCalendar cal = new GregorianCalendar();
-        Date date = new Date();
-        cal.setTime(date);
-        cal.set(GregorianCalendar.HOUR, cal.get(GregorianCalendar.HOUR)-1);
-        System.out.println(MwsUtl.getDTF().newXMLGregorianCalendar(cal));
+    public static void main2(String[] args) {
+        String runStartTime = DateUtil.format(System.currentTimeMillis(),
+                DateUtil.DATE_TIME);
+        System.out.println(runStartTime);
+    }
 
-        System.out.println(0-1);
+    public static void main3(String[] args) {
+        Set<String> set = new HashSet<>();
+        set.add("ddd");
+        set.add("ddd");
+        set.add("ccc");
+        set.add("ddd");
+        for (String s : set) {
+            System.out.println(s);
+
+        }
+
+    }
+
+    public static void main4(String[] args) {
+        Double i = Double.parseDouble("673.63");
+        Long l = Long.valueOf(i.intValue());
+        System.out.println(l);
+    }
+
+    public static void main(String[] args) {
+        int i = 7;
+        System.out.println(i -= 1);
     }
 
 }
